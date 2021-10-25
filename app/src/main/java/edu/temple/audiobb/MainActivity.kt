1678a489bbfddd8bbb0f30b1af662842a546b52a
Cyrus Ng <tun60613@temple.edu>
2021-10-25 13:42:13 -0400

    Commit 2 : Added in fragments and layouts

--- app/src/main/java/edu/temple/audiobb/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobb/MainActivity.kt
@@ -7,7 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var lis : MutableList<Book> = listOf(Book("hi","hello")) as MutableList<Book>
-        var newBooks  = BookList(lis)
+        var bContainer : MutableList<Book> = listOf(Book("Book1","Author1"),Book("Book2","Author2")
+            ,Book("Book3","Author3"),Book("Book4","Author4"),Book("Book5","Author5")
+            ,Book("Book6","Author6"),Book("Book7","Author7"),Book("Book8","Author8")
+            ,Book("Book9","Author9"),Book("Book10","Author10")) as MutableList<Book>
+        var newBooks  = BookList(bContainer)
     }
 }
